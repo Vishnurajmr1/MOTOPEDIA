7c48900b205a2fbdff0f69638c4b5e27174e999d
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-01 10:56:42 +0530

    domain added

---
+++ backend/src/domain/models/user/user.ts
@@ -0,0 +1,9 @@
+import { Role } from "../role/role";
+export interface User{
+    id:string,
+    email:string,
+    first_name:string,
+    last_name:string,
+    password_hash?:string,
+    roles?:Role[]
+}
