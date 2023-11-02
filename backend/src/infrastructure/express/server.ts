e4deb7ab08799669c416dec30bfc92845c645aa8
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-02 15:47:09 +0530

    main folder created for handling controller

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import { setUpApp } from "./setup/setup-app";
-import { setupGlobalMiddlewares } from "./setup/setup-global-middlewares";
-import { setupRoutes } from "./setup/setup-routes";
+import { setUpApp } from './setup/setup-app';
+import { setupGlobalMiddlewares } from './setup/setup-global-middlewares';
+import { setupRoutes } from './setup/setup-routes';
 import { setUpAsyncErrors } from './setup/setup-async-error';
 
-export const app=express();
+export const app = express();
 setUpApp(app);
-setupGlobalMiddlewares(app)
-setupRoutes(app)
+setupGlobalMiddlewares(app);
+setupRoutes(app);
 setUpAsyncErrors(app);
 
-const port=process.env.PORT||3000;
-app.listen(port,()=>{
-    console.log(`Server listening => http://127.0.0.1:${port}`);
-    
-})
+const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+        console.log(`Server listening => http://127.0.0.1:${port}`);
+    });
+}
