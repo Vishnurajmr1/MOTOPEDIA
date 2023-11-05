c5b619f114e24facd41059c44ed86dcb549ef610
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-05 22:05:19 +0530

    schema created using mongodb

--- backend/src/infrastructure/express/routes/sign-in.ts
+++ backend/src/infrastructure/express/routes/sign-in.ts
@@ -2,3 +2,8 @@ import { Router } from "express";
 import { expressRouteAdapter } from "../adapters/express-route-adapter";
 
 export const signInRoutes=Router();
+
+// const {signInController}=signInControllerFactory();
+
+
+// signInRoutes.post('/',expressRouteAdapter(signInController))
---
+++ backend/src/infrastructure/mongodb/schemas/users.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    last_name: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password_hash: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
