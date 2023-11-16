66fc20349be06fa7406f36b0f7a20978d6ef5dbf
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-16 10:13:23 +0530

    jwt_service_added

---
+++ frontend/src/app/auth/feature/admin-access/admin-access.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-access',
+  templateUrl: './admin-access.component.html',
+  styleUrls: ['./admin-access.component.css']
+})
+export class AdminAccessComponent {
+
+}
---
+++ frontend/src/app/shared/data-access/global/jwt.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload {
+  role: string;
+  exp: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JwtService {
+  getUserRole(token: string): string {
+    if (token) {
+      let tokenPrefix = '';
+      if (token.startsWith('access:')) {
+        tokenPrefix = 'access';
+      } else if (token.startsWith('google:')) {
+        tokenPrefix = 'google:';
+      }
+
+      if (tokenPrefix) {
+        const decodeToken: JwtPayload = jwtDecode(
+          token.substring(tokenPrefix.length)
+        );
+        return decodeToken.role;
+      }
+    }
+    return '';
+  }
+
+  private getDecodedToken(token: string): JwtPayload | undefined {
+    if (token) {
+      let tokenPrefix = '';
+      if (token.startsWith('access:')) {
+        tokenPrefix = 'access:';
+      } else if (token.startsWith('google:')) {
+        tokenPrefix = 'google:';
+      }
+
+      if (tokenPrefix) {
+        const decodedToken: JwtPayload = jwtDecode(
+          token.substring(tokenPrefix.length)
+        );
+        return decodedToken;
+      }
+    }
+    return undefined;
+  }
+
+  isTokenExpired(token: string): boolean {
+    const decodedToken = this.getDecodedToken(token);
+    const expirationTime = decodedToken?.exp;
+    const currentTimeStamp = Math.floor(Date.now() / 1000);
+    if (expirationTime !== undefined) {
+      return expirationTime < currentTimeStamp;
+    }
+    return false;
+  }
+
+  isAdmin(token: string): boolean {
+    return this.getUserRole(token) === 'ADMIN';
+  }
+  isUser(token: string): boolean {
+    return this.getUserRole(token) === 'USER';
+  }
+}
