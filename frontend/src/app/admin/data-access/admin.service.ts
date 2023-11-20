e6d4f830adf8de7708ae17633e7f9255a191c0bb
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-20 12:52:23 +0530

    user block and unblock added

--- frontend/src/app/admin/data-access/admin.service.ts
+++ frontend/src/app/admin/data-access/admin.service.ts
@@ -1,13 +1,19 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
-    providedIn:'root'
+  providedIn: 'root',
 })
-export class AdminService{
-    private http=inject(HttpClient)
-    
-    getUsers(){
-        return this.http.get('/api/user/get-all-users');
-    }
+export class AdminService {
+  private http = inject(HttpClient);
+
+  getUsers() {
+    return this.http.get('/api/user/get-all-users');
+  }
+  blockUser(userId: string, reason: string) {
+    return this.http.patch(`/api/user/block-user/${userId}`, { reason });
+  }
+  unblockUser(userId: string) {
+    return this.http.patch(`/api/user/unblock-user/${userId}`, {});
+  }
 }
--- frontend/src/app/shared/interfaces/Interface.ts
+++ frontend/src/app/shared/interfaces/Interface.ts
@@ -1,36 +1,42 @@
-export interface ISignUp{
-    firstName:string,
-    lastName:string,
-    email:string,
-    mobile:string,
-    password:string,
-    confirmPassword:string
+export interface ISignUp {
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobile: string;
+  password: string;
+  confirmPassword: string;
 }
 
-export interface ILogin{
-    email:string,
-    password:string
+export interface ILogin {
+  email: string;
+  password: string;
 }
 
-export interface IConfirmPass{
-    newPassword:string;
-    token:string
+export interface IConfirmPass {
+  newPassword: string;
+  token: string;
 }
-export interface IverifyOtp{
-otp:string;
-email?:string;
+export interface IverifyOtp {
+  otp: string;
+  email?: string;
 }
-
 
 export interface UserDoc {
-	userName?: string;
-	firstName?: string;
-	lastName?: string;
-	email: string;
-	mobile?: string;
-	password: string;
-	otp?: string;
-	profileImgUrl?: string;
-	isEmailVerified: boolean;
-    isBlocked?:boolean
+  _id?: string;
+  userName?: string;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  mobile?: string;
+  password: string;
+  otp?: string;
+  profileImgUrl?: string;
+  isEmailVerified: boolean;
+  isBlocked?: boolean;
+}
+
+export interface userResponse {
+  status: string;
+  message: string;
+  data: Object | null | undefined | string;
 }
