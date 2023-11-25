03b9eb54438a221ed67a35eb2b90023e98e6abc5
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-11-26 00:12:32 +0530

    list post ui communication added

--- frontend/src/app/posts/ui/single-post/single-post.component.ts
+++ frontend/src/app/posts/ui/single-post/single-post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IpostInterface } from 'src/app/shared/types/post.Interface';
 
 @Component({
@@ -7,5 +7,5 @@ import { IpostInterface } from 'src/app/shared/types/post.Interface';
   styleUrls: ['./single-post.component.css'],
 })
 export class SinglePostComponent {
-  posts: IpostInterface[] = [];
+  @Input() posts: IpostInterface[] = [];
 }
